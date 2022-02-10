3bd19df5565b34ee3ddfedde248604b8f8967f3e
sannimdev <inegg.apps@gmail.com>
2022-02-10 13:26:50 +0900

    CORS 대응 #13

--- src/server.js
+++ src/server.js
@@ -5,10 +5,17 @@ import express from 'express';
 import logger from 'morgan';
 import { typeDefs, resolvers } from './schema';
 import { getUser } from './users/users.utils';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 4000;
 const startServer = async () => {
+    const corsOptions = {
+        // https://cheatcode.co/tutorials/how-to-set-up-a-graphql-server-with-apollo-server-and-express
+        origin: (origin, callback) => callback(null, true),
+        credentials: true,
+    };
     const server = new ApolloServer({
+        cors: cors(corsOptions),
         typeDefs,
         resolvers,
         introspection: true,
@@ -19,12 +26,13 @@ const startServer = async () => {
     });
     await server.start();
     const app = express();
+    app.use(cors(corsOptions));
     app.use(logger('tiny'));
     app.use('/static', express.static('uploads'));
     app.use(graphqlUploadExpress());
-    server.applyMiddleware({ app });
+    server.applyMiddleware({ app, cors: corsOptions });
     await new Promise((func) => app.listen({ port: PORT }, func));
-    console.log(`🥤 Server is running on http://localhost:${PORT}/graphql`);
+    console.log(`🥤 Server is running on http://(your ip):${PORT}/graphql`);
 };
 
 startServer();
