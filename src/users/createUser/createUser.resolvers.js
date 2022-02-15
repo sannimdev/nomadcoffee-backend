42a388f3789f9f624f22fdbfb539487e28782fd2
sannimdev <inegg.apps@gmail.com>
2022-02-15 22:51:42 +0900

    사용자 아바타 이미지 받기 #21

--- src/users/createUser/createUser.resolvers.js
+++ src/users/createUser/createUser.resolvers.js
@@ -1,13 +1,14 @@
 import bcrypt from 'bcrypt';
+import GraphQLUpload from 'graphql-upload';
 import client from '../../client';
+import { uploadPhoto } from '../../lib/imgbb';
 
 export default {
+    Upload: GraphQLUpload,
     Mutation: {
-        createUser: async (
-            _,
-            { username, email, name, location, password, avatarURL, githubUsername }
-        ) => {
+        createUser: async (_, { username, email, name, location, password, avatar, githubUsername }) => {
             try {
+                // 현재 사용자가 존재하는지 체크하기
                 const existedUser = await client.user.findFirst({
                     where: {
                         OR: [{ username }, { email }],
@@ -16,21 +17,32 @@ export default {
                 if (existedUser) {
                     throw new Error('The username or email is already taken.');
                 }
+                // 사용자 등록하기
                 const encryptedPassword = await bcrypt.hash(password, 10);
-                await client.user.create({
+                const newUser = await client.user.create({
                     data: {
                         username,
                         email,
                         name,
                         location,
                         password: encryptedPassword,
-                        avatarURL,
                         githubUsername,
                     },
+                    select: { id: true },
                 });
-                return {
-                    ok: true,
-                };
+                // 아바타 존재 시
+                if (!!avatar) {
+                    const { image, thumb, medium } = await uploadPhoto(await avatar);
+                    await client.user.update({
+                        where: { id: newUser.id },
+                        data: {
+                            avatarURL: thumb?.url,
+                            avatarOriginalURL: image?.url,
+                            avatarMediumURL: medium?.url,
+                        },
+                    });
+                }
+                return { ok: true };
             } catch (error) {
                 return {
                     ok: false,
